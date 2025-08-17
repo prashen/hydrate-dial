a6954609d51945feacbdd4a62ac7b00af89bb2d5
Prashen Jeet Roy <prashen@users.noreply.github.com>
2025-08-17 20:04:28 +0000

    removed all

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import registerSW from "./registerServiceWorker";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-//registerSW();
--- src/registerServiceWorker.js
+++
@@ -1,9 +0,0 @@
-export default function registerSW() {
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .catch((err) => console.error("SW registration failed:", err));
-    });
-  }
-}
